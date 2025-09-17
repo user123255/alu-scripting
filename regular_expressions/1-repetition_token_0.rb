26e52e55172a63c5dc3d94459080128f74876abe
Nyayath Chol <n.chol@alustudent.com>
2025-09-17 06:03:12 -0700

    repetition

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbtt?n/).join
+# 1-repetition_token_0.rb
+# Usage: ./1-repetition_token_0.rb "some_string"
+
+input = ARGV[0] || ''
+match = input.scan(/hbt+n/).join
+puts match
+
+
 
 
 
